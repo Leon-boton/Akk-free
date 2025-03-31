9fc70ca663d69e8641c15be3c757b39fb3ea0204
Leon-boton <pro100shastie99@gmail.com>
2025-03-31 23:22:10 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,15 @@
-const cacheName = 'account-control-v1';
-const filesToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
-];
-
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+    caches.open("app-cache").then((cache) => {
+      return cache.addAll(["/", "/index.html", "/style.css", "/script.js"]);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
